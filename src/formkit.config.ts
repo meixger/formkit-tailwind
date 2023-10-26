39ba4ca95c9891dae24526474fe6798cfa87b9cc
Martin Meixger <mail@meixger.com>
2023-10-26 17:13:35 +0200

    docs: tailwind should not be referenced form package

--- src/formkit.config.ts
+++ src/formkit.config.ts
@@ -5,6 +5,12 @@
  * Pro inputs seem to work fine with @formkit/themes/tailwindcss/genesis.
  * import "@formkit/themes/genesis"
  */
+/* "The FormKit docs have been updated and the recommended way to install the Genesis theme is now to
+ * copy/paste the theme object into your own project as a starting point.
+ * In future releases the import object will no longer receive updates."
+ * source: https://github.com/formkit/formkit/issues/787#issuecomment-1644147166
+ * also: https://github.com/formkit/formkit/issues/560#issuecomment-1610301826
+ */
 import { DefaultConfigOptions } from '@formkit/vue'
 import { genesisIcons } from "@formkit/icons"
 import { generateClasses } from '@formkit/themes'
